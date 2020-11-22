cad0d7ec41c59e2197d8f0ff449370085a030c9e
Ashwani Kumar <ashwanikumar04@gmail.com>
2020-11-22 21:58:33 +0000

    added

--- base/BXBook.py
+++ base/BXBook.py
@@ -9,21 +9,35 @@ from surprise import Reader
 from collections import defaultdict
 import numpy as np
 
+
 class BXBook:
 
     ratings_path = './data/BX-Book-Ratings-10K.csv'
-    
+    books_path = './data/BX-Books.csv'
+
     def load_data(self):
 
         # Look for files relative to the directory we are running from
+        self.book_id_to_name_mapping = {}
 
         ratings_dataset = 0
 
-        reader = Reader(line_format='user item rating', sep=';', skip_lines=1,  rating_scale= (0,10))
+        reader = Reader(line_format='user item rating',
+                        sep=';',
+                        skip_lines=1,
+                        rating_scale=(0, 10))
 
-        ratings_dataset = Dataset.load_from_file(self.ratings_path, reader=reader)
-        
+        ratings_dataset = Dataset.load_from_file(self.ratings_path,
+                                                 reader=reader)
+        with open(self.books_path, newline='',
+                  encoding='ISO-8859-1') as csvfile:
+            book_reader = csv.reader(csvfile, delimiter=';')
+            next(book_reader)  #Skip header line
+            for row in book_reader:
+                book_id = row[0]
+                book_name = row[1]
+                self.book_id_to_name_mapping[book_id] = book_name
         return ratings_dataset
-    
+
     def get_book_name(self, book_id):
-        return ""
+        return self.book_id_to_name_mapping.get(book_id, "")
--- data/README.md
+++ data/README.md
@@ -1,3 +1,8 @@
 From: http://www2.informatik.uni-freiburg.de/~cziegler/BX/
 
 Remove `""` from Book-Ratings.csv
+
+
+```
+python3 -m base.BaseRecommenderTester
+```
